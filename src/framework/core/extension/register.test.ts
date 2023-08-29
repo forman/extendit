d3c4d7711a155f6bb4a7bba367a30c82bee2a738
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-08-29 15:46:32 +0200

    incr. coverage

--- src/framework/core/extension/register.test.ts
+++ src/framework/core/extension/register.test.ts
@@ -4,6 +4,9 @@ import { getExtension, getExtensionContext } from "@/core/store";
 import { registerExtension } from "./register";
 import { addExtensionListener } from "./listeners";
 import { ExtensionContextImpl } from "./context";
+import { ExtensionModule } from "../types";
+import { activateExtension } from "./activate";
+import { getExtensionId } from "./manifest";
 
 test("registerExtension", () => {
   addExtensionListener({
@@ -17,5 +20,35 @@ test("registerExtension", () => {
   const disposable = registerExtension(newTestManifest());
   expect(getExtension("pippo.foo")).toBeInstanceOf(Object);
   expect(getExtensionContext("pippo.foo")).toBeInstanceOf(ExtensionContextImpl);
+  disposable.dispose();
+});
+
+test("registerExtension with module", async () => {
+  const myApi = {
+    sayHello() {
+      return "Hello!";
+    },
+  };
+  const module: ExtensionModule = {
+    activate: () => {
+      return myApi;
+    },
+  };
+  const disposable = registerExtension(newTestManifest(), { module });
+  const extension = await activateExtension("pippo.foo");
+  expect(extension).toBeInstanceOf(Object);
+  expect(extension.status).toEqual("active");
+  expect(extension.exports).toBe(myApi);
+  expect(extension.exports.sayHello()).toEqual("Hello!");
+  disposable.dispose();
+});
+
+test("registerExtension with activationEvents", async () => {
+  const manifest = newTestManifest({
+    activationEvents: ["onView", "onView:${id}"],
+  });
+  const disposable = registerExtension(manifest);
+  const ctx = getExtensionContext("pippo.foo");
+  expect(ctx.activationEvents).toEqual(new Set(["onView", "onView:${id}"]));
   disposable.dispose();
 });
