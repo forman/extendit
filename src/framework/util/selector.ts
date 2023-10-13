9f5702b7ded912b10a6014bf4c977542d745138a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-10-13 13:47:17 +0200

    Added api doc

--- src/framework/util/selector.ts
+++ src/framework/util/selector.ts
@@ -1,97 +1,120 @@
 import memoizeOne from "memoize-one";
 
-export type Selector<State, Value> = (state: State) => Value;
+/**
+ * A selector is a function that selects or computes a value from a given state.
+ *
+ * @param state - The input state
+ * @returns The selected or computed value from state
+ * @typeParam State - Type of the selector's state input
+ * @typeParam Result - Type of the selector's result
+ */
+export type Selector<State, Result> = (state: State) => Result;
 
-export function createSelector<State, Value, V0>(
-  arg0: Selector<State, V0>,
-  computeResult: (arg0: V0) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  computeResult: (arg0: V0, arg1: V1) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  computeResult: (arg0: V0, arg1: V1, arg2: V2) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2, V3>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  arg3: Selector<State, V3>,
-  computeResult: (arg0: V0, arg1: V1, arg2: V2, arg3: V3) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2, V3, V4>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  arg3: Selector<State, V3>,
-  arg4: Selector<State, V4>,
-  computeResult: (arg0: V0, arg1: V1, arg2: V2, arg3: V3, arg4: V4) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2, V3, V4, V5>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  arg3: Selector<State, V3>,
-  arg4: Selector<State, V4>,
-  arg5: Selector<State, V5>,
+/**
+ * Create a new {@link Selector} based on the given selector(s) and a function
+ * that computes a new value from the result of the given selector(s).
+ *
+ * Multiple selectors may be passed (see definitions below),
+ * then their results are passed to the result computation function
+ * {@link computeResult} in the same order.
+ *
+ * @param selector - The selector function.
+ *  If multiple selectors are passed, their parameters are named
+ *  `selectorN` with `N` from `1` to `8`.
+ * @param computeResult The result computation function.
+ *  If multiple selectors are passed, the function's parameters are named
+ *  `xN` with `N` from `1` to `8`.
+ * @returns A new selector.
+ * @typeParam State - Type of the state passed to the selectors
+ *  and to the newly created selector.
+ * @typeParam Result - Type of the result of the newly created selector.
+ * @typeParam X - Type of the return value of the selector.
+ *  If multiple selectors are passed, their type parameters are named
+ *  `XN` with `N` from `1` to `8`.
+ */
+export function createSelector<State, Result, X>(
+  selector: Selector<State, X>,
+  computeResult: (x: X) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  computeResult: (x1: X1, x2: X2) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  computeResult: (x1: X1, x2: X2, x3: X3) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3, X4>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  selector4: Selector<State, X4>,
+  computeResult: (x1: X1, x2: X2, x3: X3, x4: X4) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3, X4, X5>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  selector4: Selector<State, X4>,
+  selector5: Selector<State, X5>,
+  computeResult: (x1: X1, x2: X2, x3: X3, x4: X4, x5: X5) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3, X4, X5, X6>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  selector4: Selector<State, X4>,
+  selector5: Selector<State, X5>,
+  selector6: Selector<State, X6>,
+  computeResult: (x1: X1, x2: X2, x3: X3, x4: X4, x5: X5, x6: X6) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3, X4, X5, X6, X7>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  selector4: Selector<State, X4>,
+  selector5: Selector<State, X5>,
+  selector6: Selector<State, X6>,
+  selector7: Selector<State, X7>,
   computeResult: (
-    arg0: V0,
-    arg1: V1,
-    arg2: V2,
-    arg3: V3,
-    arg4: V4,
-    arg5: V5
-  ) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2, V3, V4, V5, V6>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  arg3: Selector<State, V3>,
-  arg4: Selector<State, V4>,
-  arg5: Selector<State, V5>,
-  arg6: Selector<State, V6>,
+    x1: X1,
+    x2: X2,
+    x3: X3,
+    x4: X4,
+    x5: X5,
+    x6: X6,
+    x7: X7
+  ) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result, X1, X2, X3, X4, X5, X6, X7, X8>(
+  selector1: Selector<State, X1>,
+  selector2: Selector<State, X2>,
+  selector3: Selector<State, X3>,
+  selector4: Selector<State, X4>,
+  selector5: Selector<State, X5>,
+  selector6: Selector<State, X6>,
+  selector7: Selector<State, X7>,
+  selector8: Selector<State, X8>,
   computeResult: (
-    arg0: V0,
-    arg1: V1,
-    arg2: V2,
-    arg3: V3,
-    arg4: V4,
-    arg5: V5,
-    arg6: V6
-  ) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value, V0, V1, V2, V3, V4, V5, V6, V7>(
-  arg0: Selector<State, V0>,
-  arg1: Selector<State, V1>,
-  arg2: Selector<State, V2>,
-  arg3: Selector<State, V3>,
-  arg4: Selector<State, V4>,
-  arg5: Selector<State, V5>,
-  arg6: Selector<State, V6>,
-  arg7: Selector<State, V7>,
-  computeResult: (
-    arg0: V0,
-    arg1: V1,
-    arg2: V2,
-    arg3: V3,
-    arg4: V4,
-    arg5: V5,
-    arg6: V6,
-    arg7: V7
-  ) => Value
-): Selector<State, Value>;
-export function createSelector<State, Value>(
+    x1: X1,
+    x2: X2,
+    x3: X3,
+    x4: X4,
+    x5: X5,
+    x6: X6,
+    x7: X7,
+    x8: X8
+  ) => Result
+): Selector<State, Result>;
+export function createSelector<State, Result>(
   ...args: unknown[]
-): Selector<State, Value> {
-  const argCount = args.length;
-  const selectors = args.slice(0, argCount - 1) as Selector<State, unknown>[];
-  const computeResult = args[argCount - 1] as (...args: unknown[]) => Value;
+): Selector<State, Result> {
+  const numArgs = args.length;
+  const selectors = args.slice(0, numArgs - 1) as Selector<State, unknown>[];
+  const computeResult = args[numArgs - 1] as (...as: unknown[]) => Result;
   const computeMemoizedResult = memoizeOne((...results: unknown[]) =>
     computeResult(...results)
   );
