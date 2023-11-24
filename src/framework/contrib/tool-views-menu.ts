d4453ccaab7a9a1b1779556a8f70e76daaf6c7d3
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-11-24 09:54:50 +0100

    added useToolViewMenuItems

--- src/framework/contrib/tool-views-menu.ts
+++ src/framework/contrib/tool-views-menu.ts
@@ -4,7 +4,7 @@
  * https://opensource.org/licenses/MIT.
  */
 
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { useContributions } from "@/react";
 import type { MenuItem } from "@/contrib/menus";
 import { Logger } from "@/util/log";
@@ -19,26 +19,32 @@ export function useToolViewMenuItems(
   toolViewShowCommand: string,
   ctx: Record<string, unknown>
 ): MenuItem[] {
-  const activityBarViews = useHookConditionally(
-    useContributions<StoreToolView>,
-    false
+  const activityBarViews = useConditionalHook(
+    menuId === toolViewMenuId,
+    useContributions<StoreToolView>
   )(toolViewsPoint.id, "activityBar");
-  const secondarySideBarViews = useContributions<StoreToolView>(
-    toolViewsPoint.id,
-    "secondarySideBar"
-  );
-  const panelViews = useContributions<StoreToolView>(
-    toolViewsPoint.id,
-    "panel"
-  );
+  const secondarySideBarViews = useConditionalHook(
+    menuId === toolViewMenuId,
+    useContributions<StoreToolView>
+  )(toolViewsPoint.id, "secondarySideBar");
+  const panelViews = useConditionalHook(
+    menuId === toolViewMenuId,
+    useContributions<StoreToolView>
+  )(toolViewsPoint.id, "panel");
 
   return useMemo(() => {
     LOG.debug("Hook 'useToolViews' is recomputing");
 
     let order = 0;
 
-    function newMenuItems(views: StoreToolView[], containerId: string) {
-      return views
+    function createMenuItems(
+      toolViews: StoreToolView[] | undefined,
+      containerId: string
+    ) {
+      if (!toolViews) {
+        return [];
+      }
+      return toolViews
         .filter((view) => isValidToolView(view, ctx))
         .map((view) => ({
           id: newId(),
@@ -51,9 +57,9 @@ export function useToolViewMenuItems(
     }
 
     return [
-      ...newMenuItems(activityBarViews, "activityBar"),
-      ...newMenuItems(secondarySideBarViews, "secondarySideBar"),
-      ...newMenuItems(panelViews, "panel"),
+      ...createMenuItems(activityBarViews, "activityBar"),
+      ...createMenuItems(secondarySideBarViews, "secondarySideBar"),
+      ...createMenuItems(panelViews, "panel"),
     ];
   }, [
     activityBarViews,
@@ -68,16 +74,11 @@ function isValidToolView(view: StoreToolView, ctx: Record<string, unknown>) {
   return view.when ? view.when(ctx) : true;
 }
 
-type AnyF = (...args: unknown[]) => unknown;
-type CondF<F extends AnyF, R = ReturnType<F>> = (
-  ...args: Parameters<F>
-) => R | undefined;
-
-function useHookConditionally<F extends AnyF, R extends ReturnType<F>>(
-  hook: F,
-  condition: boolean
-): CondF<F, R> {
-  return (condition ? hook : empty) as CondF<F, R>;
+function useConditionalHook<C, A extends unknown[], R>(
+  condition: C,
+  hook: (...args: A) => R
+): ((...args: A) => R) | ((...args: A) => undefined) {
+  return condition ? hook : useNothing<A>;
 }
 
-function empty(..._args: unknown[]): undefined {}
+function useNothing<A extends unknown[]>(..._args: A): undefined {}
